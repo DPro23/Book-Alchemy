a85d8738d48a554f866d6eaac33b7e1f0d131602
DaNiLo <danilo.p2391@gmail.com>
2025-09-05 18:20:57 +0200

    Setup Flask app, connect with sqlalchemy to db and create html templates and home route

---
+++ app.py
@@ -0,0 +1,29 @@
+"""Serves the app on localhost"""
+from flask import Flask, render_template, request
+import os
+from data_models import db, Author, Book
+
+# Init Flask app and connect to database
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
+db.init_app(app)
+
+
+@app.route('/')
+def home():
+    """Renders the home page"""
+    all_books = db.session.query(Book).all()
+    return render_template('home.html', books=all_books)
+
+
+if __name__ == "__main__":
+    """
+    Resets and create tables
+    ‼️To comment after first usage!!
+    Then run the Flask app
+    """
+    #with app.app_context():
+    #    db.drop_all()
+    #    db.create_all()
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ data_models.py
@@ -0,0 +1,39 @@
+""""Define data models"""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Author(db.Model):
+    """author model"""
+    __tablename__ = 'author'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    birth_date = db.Column(db.Date)
+    date_of_death = db.Column(db.Date, nullable=True)
+
+
+    def __repr__(self):
+        return f"Author({self.name}, {self.birth_date}, {self.date_of_death})"
+
+
+    def __str__(self):
+        return f"Author({self.name}, {self.birth_date}, {self.date_of_death})"
+
+
+class Book(db.Model):
+    """book model"""
+    __tablename__ = 'book'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String)
+    title = db.Column(db.String)
+    publication_year = db.Column(db.Integer)
+    author_id = db.Column(db.Integer, db.ForeignKey(Author.id))
+    author = db.relationship(Author)
+
+
+    def __repr__(self):
+        return f"Book({self.isbn}, {self.title}, {self.publication_year})"
+
+
+    def __str__(self):
+        return f"Book({self.isbn}, {self.title}, {self.publication_year})"
